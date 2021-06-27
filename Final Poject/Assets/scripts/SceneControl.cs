f163493839bd62d7954c17aaa9180807f1539a13
Krassi Petrov <IMYanev18@codingburgas.bg>
2021-06-27 14:50:24 +0200

    Adding final details to level 1
    Fix #36
    Fix #37

--- Final Poject/Assets/scripts/SceneControl.cs
+++ Final Poject/Assets/scripts/SceneControl.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,16 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneControl : MonoBehaviour
 {
-    public int index;
     public string levelName;
 
    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(index);
-
-            //SceneManager.LoadScene(levelName);
+            SceneManager.LoadScene(levelName);
         }
     }
 }
